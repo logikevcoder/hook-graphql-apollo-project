57b2515a00231ac7514172743e4b10baba2e8b75
Kevin Napier <Kevin.Napier@rotary.org>
2020-03-31 13:06:56 -0500

    created all the functionality to validate the googleUser data coming from the login. Consists of adding graphqlclient and GoogleLogin in the login page, passing the onSuccess data to grab the token ID passed in by the signed in user, validating that token in the server.js file by grabbing the token and passing the headers.authorization, authentication verified by the newly created userController creating the new userSchema file

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -1,18 +1,46 @@
-import React from "react";
-import { withStyles } from "@material-ui/core/styles";
+import React from 'react';
+import { GraphQLClient } from 'graphql-request';
+import { GoogleLogin } from 'react-google-login';
+import { withStyles } from '@material-ui/core/styles';
 // import Typography from "@material-ui/core/Typography";
 
+const ME_QUERY = `{
+  me {
+    _id
+    name
+    email
+    picture
+  }
+}`;
+
 const Login = ({ classes }) => {
-  return <div>Login</div>;
+  const onSuccess = async googleUser => {
+    const idToken = googleUser.getAuthResponse().id_token;
+    const client = new GraphQLClient('http://localhost:4000/graphql', {
+      headers: { authorization: idToken } // pass in the idToken from the google login
+    });
+    const data = await client.request(ME_QUERY); // ask for current User data AFTER user logs in
+    console.log({ data });
+  };
+
+  return (
+    <GoogleLogin
+      clientId='4343767367-fku2cqh6vc589omut9ua87mjfu5tbmr1.apps.googleusercontent.com'
+      onSuccess={onSuccess}
+      isSignedIn={true}
+    >
+      Login
+    </GoogleLogin>
+  );
 };
 
 const styles = {
   root: {
-    height: "100vh",
-    display: "flex",
-    justifyContent: "center",
-    flexDirection: "column",
-    alignItems: "center"
+    height: '100vh',
+    display: 'flex',
+    justifyContent: 'center',
+    flexDirection: 'column',
+    alignItems: 'center'
   }
 };
 
